8945da275d67b2186856a3a9ea7df9c7690a6c08
Wesley Patrick <wesley-p.m@hotmail.com>
2017-03-02 22:07:35 -0300

    eventos js

---
+++ Day10/js/main.js
@@ -0,0 +1,8 @@
+
+
+const one = document.getElementsByClassName('num1').value = 1;
+
+window.addEventListener('click',() => {
+		const a = document.getElementsByClassName('visor').value = one;
+		console.log(a);
+});
