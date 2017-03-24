add11bab2fa4823b7f86d646e9a014b9d332d609
Wesley Patrick <wesley-p.m@hotmail.com>
2017-03-23 23:19:22 -0300

    form PHP

---
+++ day20/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<form action="js/main.php">
+		<table>
+			<tr>
+				<td>
+					<label>
+						Email : <input type="text" name="email">
+					</label>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label>
+						Senha : <input type="password" name="senha">
+					</label>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<input type="submit" value="Cadastrar">
+				</td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
